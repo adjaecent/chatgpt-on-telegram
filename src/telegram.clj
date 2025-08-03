3eb8ceddddecfcc4f7bc2c48834ef381196c6b4d
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2025-08-03 17:04:54 +0530

    Switch to virtual threads (#1)
    Co-authored-by: Akshay Gupta <kitallis@gmail.com>

--- src/telegram.clj
+++ src/telegram.clj
@@ -2,9 +2,12 @@
   (:require [mount.core :refer [defstate]]
             [openai]
             [config :as c]
+            [threads :as t]
             [sundry :refer [when-let*]])
   (:import
-   [org.telegram.telegrambots.client.okhttp OkHttpTelegramClient]
+   [org.telegram.telegrambots.client.jetty JettyTelegramClient]
+   [org.eclipse.jetty.client HttpClient]
+   [org.eclipse.jetty.client.transport HttpClientTransportOverHTTP]
    [org.telegram.telegrambots.meta.exceptions TelegramApiException]
    [org.telegram.telegrambots.meta.api.methods.send SendMessage]
    [org.telegram.telegrambots.meta.api.methods.updatingmessages EditMessageText]
@@ -14,7 +17,39 @@
    [org.telegram.telegrambots.longpolling.util LongPollingSingleThreadUpdateConsumer]
    [org.telegram.telegrambots.longpolling TelegramBotsLongPollingApplication]))
 
+(defonce max-requests 100)
+(defonce max-connections 5)
+(defonce connect-timeout 5000)
 (def bot-token (-> (c/fetch) (c/telegram-bot-key)))
+(declare create-bot)
+
+(defn start-http-client []
+  (println "Starting Telegram HTTP client...")
+  (-> (HttpClient. (HttpClientTransportOverHTTP. 1))
+      (doto
+        (.setMaxConnectionsPerDestination max-connections)
+        (.setMaxRequestsQueuedPerDestination max-requests)
+        (.setConnectTimeout connect-timeout)
+        (.setExecutor (t/jetty-vthread-pool))
+        (.start))
+      (JettyTelegramClient. bot-token)))
+
+(defn stop-http-client [client]
+  (println "Stopping Telegram HTTP client...")
+  (.close client))
+
+(defn start-consumer []
+  (let [bot (TelegramBotsLongPollingApplication.)]
+    (println "Starting Telegram consumer...")
+    (.registerBot bot bot-token (create-bot bot-token))
+    bot))
+
+(defn stop-consumer [client]
+  (println "Stopping Telegram consumer...")
+  (.close client))
+
+(defstate http-client :start (start-http-client) :stop (stop-http-client http-client))
+(defstate consumer :start (start-consumer) :stop (stop-consumer consumer))
 
 (defn typing-action [^String id]
   (-> (SendChatAction/builder)
@@ -54,36 +89,20 @@
         (exec client (edit-msg op-chat-id @self-response-chat-id chunk))))))
 
 (defn create-bot [token]
-  (let [client (OkHttpTelegramClient. token)]
-    (reify LongPollingSingleThreadUpdateConsumer
-      (^void consume [^LongPollingSingleThreadUpdateConsumer _ ^Update msg-update]
-       (when-let* [_                     (.hasMessage msg-update)
-                   msg                   (.getMessage msg-update)
-                   msg-id                (.getMessageId msg)
-                   _                     (.hasText msg)
-                   chat-id               (.getChatId msg)
-                   msg-contents          (.getText msg)
-                   self-response-chat-id (atom nil)]
-         (try
-           (openai/chat-completion-streaming
-            openai/gpt-4
-            msg-contents
-            (partial chunked-response client (str chat-id) msg-id self-response-chat-id))
-           (catch TelegramApiException e
-             (.printStackTrace e))))
-       nil)))) ;; Explicitly return nil for void method
-
-(defn start []
-  (let [bot (TelegramBotsLongPollingApplication.)]
-    (.registerBot bot bot-token (create-bot bot-token))
-    (println "Telegram bot successfully started!")
-    bot))
-
-(defn stop [client]
-  (println "Telegram bot stopping...")
-  (.close client))
-
-(defstate client
-  :start (start)
-  :stop (stop client))
-
+  (reify LongPollingSingleThreadUpdateConsumer
+    (^void consume [^LongPollingSingleThreadUpdateConsumer _ ^Update msg-update]
+     (when-let* [_                     (.hasMessage msg-update)
+                 msg                   (.getMessage msg-update)
+                 msg-id                (.getMessageId msg)
+                 _                     (.hasText msg)
+                 chat-id               (.getChatId msg)
+                 msg-contents          (.getText msg)
+                 self-response-chat-id (atom nil)]
+       (try
+         (openai/chat-completion-streaming
+          openai/gpt-4
+          msg-contents
+          (partial chunked-response http-client (str chat-id) msg-id self-response-chat-id))
+         (catch TelegramApiException e
+           (.printStackTrace e))))
+     nil))) ;; Explicitly return nil for void method
