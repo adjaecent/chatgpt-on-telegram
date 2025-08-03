14e831a1be54c0d9ee3196d9b749c74a73ba45b1
Akshay Gupta <kitallis@gmail.com>
2025-08-03 16:51:28 +0530

    Add virtual threads everywhere

--- src/threads.clj
+++ src/threads.clj
@@ -1,4 +1,17 @@
 (ns threads
-  (:import [java.util.concurrent Executors]))
+  (:import [java.util.concurrent Executors]
+           [org.eclipse.jetty.util VirtualThreads]
+           [org.eclipse.jetty.util.thread VirtualThreadPool]))
 
-(defonce vthread-executor (Executors/newVirtualThreadPerTaskExecutor))
+(defonce vthread-executor
+  ^{:doc "These are run on platform threads (common, carrier fork-join pool"}
+  (Executors/newVirtualThreadPerTaskExecutor))
+
+(defn jetty-vthread-pool
+  "Pure virtual threads. Not run on platform threads.
+  See https://jetty.org/docs/jetty/12/programming-guide/arch/threads.html#thread-pool-virtual-threads for more info
+  VirtualThreadPool doesn't actually pool, so we enforce a max limit"
+  []
+  (doto (VirtualThreadPool.)
+    (.setMaxThreads 200)
+    (.setName "jetty-vthread")))
