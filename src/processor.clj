37fffa5b696770502909a5ed1b7cc0f32c4b8903
Akshay Gupta <kitallis@gmail.com>
2025-08-04 15:13:17 +0530

    rewrite response processor a bit clearly

--- src/processor.clj
+++ src/processor.clj
@@ -3,16 +3,18 @@
             [session]
             [telegram]))
 
-(defn process-response
+(defn- process-response
   ([session-id chunk]
    (process-response session-id chunk false))
   ([session-id chunk eof?]
    (when-not (empty? chunk)
-     (let [{:keys [chat-id current-user-message-id current-response-message-id] :as session} (session/fetch session-id)]
-       (if (nil? current-response-message-id)
-         (let [response-chat-id (telegram/send-first-response chat-id current-user-message-id chunk (boolean eof?))]
-           (session/write chat-id (assoc session :current-response-message-id response-chat-id)))
-         (telegram/send-updated-response current-response-message-id chat-id chunk (boolean eof?)))))))
+     (let [{:keys [chat-id current-user-message-id] :as session} (session/fetch session-id)
+           eof? (boolean eof?)]
+       (if-let [current-response-message-id (:current-response-message-id session)]
+         (telegram/send-updated-response current-response-message-id chat-id chunk eof?)
+         (->> (telegram/send-first-response chat-id current-user-message-id chunk eof?)
+              (assoc session :current-response-message-id)
+              (session/write chat-id)))))))
 
 (defn process-user-input [chat-id user-msg-id prompt-content]
   (let [session (-> (or (session/fetch chat-id)
@@ -22,4 +24,6 @@
                     (assoc :current-response-message-id nil)
                     (update :messages conj {:role :user :content prompt-content}))]
     (session/write chat-id session)
-    (openai/chat-completion-streaming :general prompt-content (partial process-response chat-id))))
+    (openai/chat-completion-streaming :general
+                                      prompt-content
+                                      (partial process-response chat-id))))
